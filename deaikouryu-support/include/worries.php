ec934a6e295aadd7a9bf307a8407c7086f82f783
神園巧真 <>
2023-09-30 08:59:01 +0900

    『こんなお悩みありませんか』の部分を構築しました。

---
+++ deaikouryu-support/include/worries.php
@@ -0,0 +1,14 @@
+<section class="worries">             
+<div class="worries__inner">
+  <div class="worries__desc">
+  <h3>こんなお悩みありませんか？</h3>
+  <ul>
+    <li>従業員の男女比が大きく、出会いの場がない</li>
+    <li>結婚支援に取り組みたいが、ノウハウがない</li>
+    <li>独身従業員のモチベーションをあげたい</li>
+  </ul>
+  </div>
+  
+
+                </div>
+              </section>
