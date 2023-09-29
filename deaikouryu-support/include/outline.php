025b8064284836b826d01a1b452a189ffa67479d
神園巧真 <>
2023-09-30 08:57:24 +0900

    事業内容のセクションの構築をしました

---
+++ deaikouryu-support/include/outline.php
@@ -0,0 +1,17 @@
+<section class="outline">
+  <h2 class="outline__title">
+    <span class="wrap"><span>事業<br>概要</span></span>いつまでも夢や希望を抱ける<br class="sp"> 鹿児島市であるために
+  </h2>
+                
+<div class="outline__inner">
+  <div class="outline__desc">
+  <p>鹿児島市では、結婚を望む若い世代が、希望する時期に結婚できるよう、企業・団体間の交流による、職業や業種の枠を超えた出会いの場を提供するサポートセンターを設置しました。</p>
+  <p>サポートセンターでは、皆様のご要望に応じて、イベントに向けた企業・団体間のマッチングや、イベント企画や運営のバックアップを行います。</p>
+  </div>
+  <div class="outline__img">
+    <p class="outline__img-tit">独身従業員向け出会いイベント開催イメージ</p>
+    <img src="<?= get_template_directory_uri(); ?>/images/deai-image.png" alt="イベント開催イメージ画像">
+  </div>
+
+                </div>
+              </section>
