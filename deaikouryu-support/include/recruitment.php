def0ade9bdd82951a292b0bf48b399b6d7dc248e
神園巧真 <>
2023-09-30 09:00:59 +0900

    登録団体募集のセクションを構築しました。

---
+++ deaikouryu-support/include/recruitment.php
@@ -0,0 +1,51 @@
+<section class="recruitment">             
+<div class="recruitment__desc">
+  <div class="recruitment__desc-top">
+    <p>市の取組に趣旨に賛同し、従業員の出会いを応援する取組を行う企業・団体を広く募集します。</p>
+    <h3>登録団体を随時募集します。</h3>
+     <p>下記表示の「サポートあ団体に登録」ボタンより、ぜひ登録ください。</p>
+  </div>
+  <div class="recruitment__desc-second">
+    <h4>登録団体について</h4>
+     <p>下記表示の「サポートあ団体に登録」ボタンより、ぜひ登録ください。</p>
+  </div>
+
+  <div class="recruitment__desc-heart">
+<div class="recruitment__desc-heart-left">
+  <div class="recruitment__desc-heart-img">
+    <img src="<?= get_template_directory_uri(); ?>/images/group-staff.png" alt="">
+  </div>
+  <div class="recruitment__desc-heart-txts">
+  <h5 class="recruitment__desc-heart-tit">自社の従業員の出会い・交流を支援</h5>
+  <ul>
+    <li>従業員への出会いイベントの情報提供</li>
+    <li>スタッフサポート団体間での出会いイベントの実施協力・など</li>
+  </ul>
+  </div>
+</div>
+<div class="recruitment__desc-heart-right">
+ <div class="recruitment__desc-heart-img">
+    <img src="<?= get_template_directory_uri(); ?>/images/group-staff.png" alt="">
+  </div>
+  <div class="recruitment__desc-heart-txts">
+  <h5 class="recruitment__desc-heart-tit">イベントの企画・実施などによりスタッフサポート団体の活動を支援</h5>
+  <ul>
+    <li>スタッフサポート団体の独身従業員を対象とした、出会いや交流を応援する取組み（出会いイベントの開催、会場提供、飲食提供など。それぞれの団体の得意な領域でサポートしていたただきます））</li>
+    <li>市が実施する結婚支援の取組の周知協力など</li>
+  </ul>
+  </div>
+</div>
+<div class="recruitment__desc-entry">
+    <h4><span>&#9312;</span> 登録資格</h4>
+     <p>市内に事務所又は事業所等がある企業・団体で、本事業の趣旨に賛同し、自社の独身従業員の出会いの応援や、出会いの場の提供が可能であること。（結婚相談、お見合い、出会い及び結婚の斡旋等を業とする企業等は除きます）</p>
+  </div>
+  <div class="recruitment__desc-entry">
+    <h4><span>&#9313;</span>登録方法</h4>
+     <p>実施要領をご覧いただき下の応募フォーム（「サポート団体に登録」ボタン）よりご登録ください。（所定の様式による登録も受け付けております。お問い合わせフォームからご連絡ください。）</p>
+  </div>
+
+
+
+  </div>
+                </div>
+              </section>
