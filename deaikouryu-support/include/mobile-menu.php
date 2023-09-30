8a1c6115710333d7c737c9eb3569b7a4efbafa13
神園巧真 <>
2023-09-30 15:56:30 +0900

    モバイルメニュー部分の修正。
    アンカーリンクのボタンをクリックした時もメニューが閉じるように設定

--- deaikouryu-support/include/mobile-menu.php
+++ deaikouryu-support/include/mobile-menu.php
@@ -1,9 +1,9 @@
 <nav class="mobile-menu">
-        <div class="logo">
-         
-          <span>株式会社</span>
-          <span>川原建設</span>
-        </div>
+<div class="logo item">
+              <a href="<?= esc_url(home_url('/')); ?>">
+             <img src="<?= get_template_directory_uri(); ?>/images/logo.png" alt="logo画像" srcset="">
+             </a>
+</div>
         <?php
                 $args=[
                   'menu'=>'mobile-nav',
--- deaikouryu-support/scripts/libs/mobile-menu.js
+++ deaikouryu-support/scripts/libs/mobile-menu.js
@@ -4,13 +4,15 @@ class MobileMenu {
     this.DOM.btn = document.querySelector(".mobile-menu__btn");
     this.DOM.cover = document.querySelector(".mobile-menu__cover");
     this.DOM.container = document.querySelector("#global-container");
+    this.DOM.menu = document.querySelectorAll(".mobile-menu__item > a");
     this.eventType = this._getEventType();
     this._addEvent();
   }
 
   _getEventType() {
-    const isTouchCapable = "ontouchstart" in window ||
-    (window.DocumentTouch && document instanceof DocumentTouch);
+    const isTouchCapable =
+      "ontouchstart" in window ||
+      (window.DocumentTouch && document instanceof DocumentTouch);
 
     return isTouchCapable ? "touchstart" : "click";
   }
@@ -22,5 +24,8 @@ class MobileMenu {
   _addEvent() {
     this.DOM.btn.addEventListener(this.eventType, this._toggle.bind(this));
     this.DOM.cover.addEventListener(this.eventType, this._toggle.bind(this));
+    this.DOM.menu.forEach((element) => {
+      element.addEventListener("click", this._toggle.bind(this));
+    });
   }
 }
