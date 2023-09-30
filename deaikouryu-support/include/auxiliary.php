528bb5e2f3363437007fdf6ecf6bc86fcc39e880
神園巧真 <>
2023-09-30 09:03:00 +0900

    特典募集セクションと開催費補助のセクションの構築をしました

---
+++ deaikouryu-support/include/auxiliary.php
@@ -0,0 +1,29 @@
+<section class="auxiliary">             
+<div class="auxiliary__inner">
+  <div class="auxiliary__desc">
+  <h3>出会いイベントの開催経費を補助します！</h3>
+  <div class="auxiliary__desc-txt">
+  <p>鹿児島市では、登録団体等が開催する登録団体間の出会いイベントの開催経費を一部補助します。
+  <span>（予算に限りがございますので、お早目にご相談ください。）</span>
+  </p>
+ 
+  </div>
+  <ul>
+    <li>
+      <span>対象となるイベント</span>
+      <p>登録団体等が行う、募集定員２０名以上の独身男女を対象とした出会いイベント</p>
+    </li>
+    <li>
+      <span>上限額</span>
+      <p>１回の出会いイベント実施にあたり１０万円</p>
+      <p>※この補助金は、事業を開始する前に申請が必要です。</p>
+    </li>
+   
+  </ul>
+  <div class="auxiliary__btn">
+    <span>※お申込み・詳細は、コチラから</span>
+    <a class="btn" href="">鹿児島市ホームページへ</a>
+  </div>
+  </div>
+                </div>
+              </section>
---
+++ deaikouryu-support/include/benefits.php
@@ -0,0 +1,21 @@
+<section class="benefits">             
+<div class="benefits__inner">
+  
+<div class="benefits__tit">
+  <h3>企業・団体からの特典も募集中！</h3>
+  </div>
+  <div class="benefits__desc">
+  <p>出会い、婚活を応援する気運をさらに高めていくため、当事業で活用できるプレゼント・サービスの募集を行っています。</p>
+  <p>※ご提供いただくサービスの内容は、ご協力いただける範囲内で決定してください（食事券、割引、粗品の進呈など）。</p>
+  </div>
+
+                </div>
+                <div class="benefits__pdf">
+                  <div class="benefits__pdf-btn">
+                    <a href="">事業実施要領 (PDF 128k)</a>
+                  </div>
+                  <div class="benefits__pdf-btn">
+                    <a href="">リーフレット (PDF 128k)</a>
+                  </div>
+                </div>
+              </section>
