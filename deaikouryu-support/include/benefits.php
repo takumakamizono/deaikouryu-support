eccbcada8238a64ca2d554cc97ef62e57c2dd72f
神園巧真 <>
2023-10-06 10:12:09 +0900

    特典募集の部分の文章の改行位置の設定。PDFリンクの表記の修正

--- deaikouryu-support/include/benefits.php
+++ deaikouryu-support/include/benefits.php
@@ -5,17 +5,17 @@
   <h3>企業・団体からの特典も募集中！</h3>
   </div>
   <div class="benefits__desc">
-  <p>出会い、婚活を応援する気運をさらに高めていくため、当事業で活用できるプレゼント・サービスの募集を行っています。</p>
+  <p>出会い、婚活を応援する気運をさらに高めていくため、<br class="sp02">当事業で活用できるプレゼント・サービスの募集を行っています。</p>
   <p>※ご提供いただくサービスの内容は、ご協力いただける範囲内で決定してください（食事券、割引、粗品の進呈など）。</p>
   </div>
 
                 </div>
                 <div class="benefits__pdf">
                   <div class="benefits__pdf-btn">
-                    <a href="">事業実施要領 (PDF 128k)</a>
+                    <a href="">詳細はコチラ(事業実施要領)PDF</a>
                   </div>
                   <div class="benefits__pdf-btn">
-                    <a href="">リーフレット (PDF 128k)</a>
+                    <a href="">リーフレット PDF</a>
                   </div>
                 </div>
               </section>
