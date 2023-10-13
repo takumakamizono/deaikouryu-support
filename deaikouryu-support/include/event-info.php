7ce72c66edf3c4e6c88a64cecb680b222a633131
神園巧真 <>
2023-10-13 11:03:58 +0900

    イベント情報セクションは管理画面からカスタムHTMLに移植しました。

--- deaikouryu-support/include/event-info.php
+++ deaikouryu-support/include/event-info.php
@@ -1,24 +1,5 @@
-<section id="event-info" class="event-info">             
-<div class="event-info__inner">
-  <h3 class="section-titles">イベント情報</h3>
-  <div class="event-info__desc">
-    <div class="event-info__desc-left">
-      <span>2023.12月1日（金）開催</span>
-      <h4>担当者向けセミナー＆情報交換会</h4>
-      <a class="btn" href=""><span>お申し込みはこちら</span><span>Googleフォーム</span></a>
-      <div class="event-info__pdf-btn">
-                    <a tabindex="-1">詳細はコチラPDF</a>
-                  </div>
-    </div>
- 
-    <div class="event-info__desc-right">
-    <span>2023.2月24日（土）開催</span>
-      <h4>従業員サポート団体　全体交流会</h4>
-      <a class="btn" href=""><span>12月下旬より申込開始予定</span></a>
-      <div class="event-info__pdf-btn">
-                    <a tabindex="-1">チラシPDF</a>
-                  </div>
-    </div>
-  </div>
-                </div>
-              </section>
+<?php
+  $page_id = get_page_by_path('event-info');  //〇〇スラッグ名が入ります
+  $page = get_post( $page_id );
+    echo $page -> post_content;  //本文を取得
+?>
