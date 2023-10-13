c5db1837b62affee0f41926a8fe466d9f46fefe9
神園巧真 <>
2023-10-13 11:08:15 +0900

    サポート団体登録のURLを設定しました。

--- deaikouryu-support/include/join-banner.php
+++ deaikouryu-support/include/join-banner.php
@@ -2,7 +2,7 @@
 <div class="join-banner__inner">
    
     <div class="join-banner__btn">
-   <a href=""target="_blank" rel="noopener noreferrer" class="btn">サポート団体に登録</a>
+   <a href="https://docs.google.com/forms/d/1afhAbpyd0YJDkMKtBfzZkUkhJsabuswI3_njOACF_h4/edit"target="_blank" rel="noopener noreferrer" class="btn">サポート団体に登録</a>
     </div>
     
 
