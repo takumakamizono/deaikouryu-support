921f50b2bf4e834d0d59df9b07ffd02ec27e4965
神園巧真 <>
2023-10-13 10:49:16 +0900

    不要な記述を削除
    管理画面のブロックエディタの非表示の設定

--- deaikouryu-support/functions.php
+++ deaikouryu-support/functions.php
@@ -122,12 +122,3 @@ add_filter( 'excerpt_length', 'twpp_change_excerpt_length', 999 );
 
 
 
-add_filter('use_block_editor_for_post',function($use_block_editor,$post){
-  if($post->post_type==='page'){
-      if(in_array($post->post_name,['top_slide','sub_topimg','sitemap'])){
-          remove_post_type_support('page','editor');
-          return false;
-      }
-  }
-  return $use_block_editor;
-},10,2);
