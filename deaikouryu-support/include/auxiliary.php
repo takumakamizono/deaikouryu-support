5e0f3cfdee7fbbd475f68bd23e01e894ff755b4f
神園巧真 <>
2023-10-13 10:56:23 +0900

    開催経費補助のHPリンクの設定とテキストの改行位置の設定をしました

--- deaikouryu-support/include/auxiliary.php
+++ deaikouryu-support/include/auxiliary.php
@@ -1,7 +1,7 @@
 <section class="auxiliary">             
 <div class="auxiliary__inner">
   <div class="auxiliary__desc">
-  <h3>出会いイベントの開催経費を補助します！</h3>
+  <h3>出会いイベントの開催経費を<br class="sp-sm">補助します！</h3>
   <div class="auxiliary__desc-txt">
   <p>鹿児島市では、登録団体等が開催する登録団体間の出会いイベントの開催経費を一部補助します。
   <span>（予算に限りがございますので、お早目にご相談ください。）</span>
@@ -22,7 +22,7 @@
   </ul>
   <div class="auxiliary__btn">
     <span>※お申込み・詳細は、コチラから</span>
-    <a class="btn" href="">鹿児島市HPへ</a>
+    <a class="btn" href="https://www.city.kagoshima.lg.jp/kosodate/kosodate/wedding/kigyoudantaikankouryu.html" target="_blank" rel="noopener">鹿児島市HPへ</a>
   </div>
   </div>
                 </div>
